e0ece21813c94ec3d9b2310247eb175000615ab6
Michael McShinsky <mmcshinsky@gmail.com>
2021-10-16 11:41:26 -0600

    Updates to TabSet and TabItem components

--- src/navigation/TabItem.tsx
+++ src/navigation/TabItem.tsx
@@ -7,6 +7,8 @@ export interface TabItemProps {
   className?: string;
   custom?: boolean;
   tag: 'li' | 'a' | 'div' | 'input';
+  border?: boolean;
+  background?: boolean;
 }
 
 export function TabItem({
@@ -15,22 +17,36 @@ export function TabItem({
   className,
   custom,
   tag,
+  border,
+  background,
   ...props
 }: TabItemProps) {
   const classes = classNames(
-    'block cursor-pointer leading-none outline-none',
-    'border-b-2 border-solid',
-    'hover:bg-gray-100 active:bg-gray-100 focus:bg-gray-100',
-    !active && 'hover:border-gray-500 active:border-gray-500 focus:border-gray-500',
-    active ? 'active border-blue-500' : 'border-transparent',
-    !custom && 'p-2',
+    'sui--tab-item',
+    'block cursor-pointer leading-none outline-none text-gray-500',
+    border && 'border-b-2 border-solid',
+    {
+      'hover:text-black active:text-black focus:text-black':
+        !background && !active,
+    },
+    {
+      'hover:bg-gray-100 active:bg-gray-100 focus:bg-gray-100':
+        !active && background,
+    },
+    {
+      'bg-blue-100': active && background,
+    },
+    active ? 'active border-blue-500 text-blue-500' : 'border-transparent',
+    { 'mx-2 py-3': !custom && !background },
+    { 'p-3 rounded-md': background },
+    { 'rounded-b-none': background && border },
     className
   );
 
   const Tag = tag;
 
   return (
-    <Tag {...props} className={classes} tabIndex={0}>
+    <Tag className={classes} tabIndex={0} {...props}>
       {children}
     </Tag>
   );
--- src/navigation/TabSet.tsx
+++ src/navigation/TabSet.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import classNames from 'classnames';
+import { TabItem, TabItemProps } from './TabItem';
 
 export interface TabSetProps {
   vertical?: boolean;
@@ -7,7 +8,13 @@ export interface TabSetProps {
   className?: string;
   rtl?: boolean;
   tag: 'nav' | 'ul' | 'div';
-  tabs?: string[];
+  tabs?: TabItem[];
+  border?: boolean;
+  background?: boolean;
+}
+
+interface TabItemMappedProps extends TabItemProps {
+  key?: string;
 }
 
 export function TabSet({
@@ -17,9 +24,12 @@ export function TabSet({
   tabs,
   tag: Tag,
   vertical,
+  border,
+  background,
   ...props
 }: TabSetProps) {
   const classes = classNames(
+    'sui--tab-set',
     'flex flex-wrap list-none',
     rtl && (vertical ? 'flex-col-reverse' : 'flex-row-reverse'),
     vertical && 'flex-col'
@@ -29,13 +39,16 @@ export function TabSet({
     .filter(Boolean)
     .map((child: any) => {
       if (child?.type?.displayName?.includes('TabItem')) {
-        return React.cloneElement(child);
+        return React.cloneElement(child, { border, background });
       }
       return React.cloneElement(child);
     });
 
   return (
     <Tag {...props} className={classes}>
+      {tabs?.map(({ key, ...tab }: TabItemMappedProps) => (
+        <TabItem key={key} {...tab} />
+      ))}
       {renderedChildren}
     </Tag>
   );
