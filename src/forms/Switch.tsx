3355d073c157e387e0d91f9323c002060a1ad4ac
Michael McShinsky <mmcshinsky@gmail.com>
2021-10-16 18:16:15 -0600

    fix types and refs

--- src/forms/Switch.tsx
+++ src/forms/Switch.tsx
@@ -1,70 +1,80 @@
-import React, { forwardRef, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import classNames from 'classnames';
-import { InputProps } from './Input';
+import { LabelProps } from './Label';
 
-export interface SwitchProps {
+export interface SwitchProps extends LabelProps {
   checked?: boolean;
   children?: ReactNode;
   color?: string;
-  inputsProps?: InputProps;
-  label?: ReactNode;
+  inputsProps?: any;
   onChange?: (e: React.FormEvent<HTMLInputElement>) => void;
   right?: boolean;
-  size?: 'sm' | 'md' | 'lg';
 }
 
-export const Switch = forwardRef<HTMLElement, SwitchProps>(
-  (
-    { label, color, checked, onChange, inputsProps, right, size, ...props },
-    ref
-  ) => {
-    const spanClasses = classNames(
-      'flex flex-shrink-0 items-center p-1',
-      'duration-300 ease-in-out bg-gray-300 rounded-full',
-      `peer-checked:bg-${color}-500`,
-      'after:bg-white after:rounded-full',
-      'after:shadow-md after:duration-300',
-      {
-        'w-11 h-7 after:w-5 after:h-5 peer-checked:after:translate-x-4':
-          size === 'sm',
-      },
-      {
-        'w-14 h-8 after:w-6 after:h-6 peer-checked:after:translate-x-6':
-          size === 'md',
-      },
-      {
-        'w-16 h-10 after:w-8 after:h-8 peer-checked:after:translate-x-6':
-          size === 'lg',
-      }
-    );
+export function Switch({
+  checked,
+  className,
+  children,
+  color,
+  disabled,
+  inputsProps,
+  onChange,
+  required,
+  right,
+  size,
+  ...props
+}: SwitchProps) {
+  const spanClasses = classNames(
+    'flex flex-shrink-0 items-center p-1',
+    'duration-300 ease-in-out bg-gray-300 rounded-full',
+    `peer-checked:bg-${color}-500`,
+    'after:bg-white after:rounded-full',
+    'after:shadow-md after:duration-300',
+    {
+      'w-11 h-7 after:w-5 after:h-5 peer-checked:after:translate-x-4':
+        size === 'sm',
+    },
+    {
+      'w-14 h-8 after:w-6 after:h-6 peer-checked:after:translate-x-6':
+        size === 'md',
+    },
+    {
+      'w-16 h-10 after:w-8 after:h-8 peer-checked:after:translate-x-6':
+        size === 'lg',
+    }
+  );
 
-    const labelClasses = classNames(
-      'text-gray-700',
-      size === 'sm' && 'text-sm',
-      size === 'md' && 'text-base',
-      size === 'lg' && 'text-lg',
-      right ? 'pl-2' : 'pr-2'
-    );
+  const labelClasses = classNames(
+    'relative inline-flex items-center justify-between cursor-pointer',
+    { 'opacity-50 cursor-not-allowed': disabled },
+    className
+  );
 
-    return (
-      <label
-        className="relative inline-flex items-center justify-between cursor-pointer"
-        {...props}
-      >
-        {!right && <span className={labelClasses}>{label}</span>}
-        <input
-          type="checkbox"
-          className="absolute hidden w-full h-full -translate-x-1/2 rounded-md appearance-none left-1/2 peer"
-          checked={checked}
-          onChange={onChange}
-          {...inputsProps}
-        />
-        <span className={spanClasses}></span>
-        {right && <span className={labelClasses}>{label}</span>}
-      </label>
-    );
-  }
-);
+  const labelInnnerClasses = classNames(
+    'text-gray-700',
+    { 'text-sm': size === 'sm' },
+    { 'text-base': size === 'md' },
+    { 'text-lg': size === 'lg' },
+    right ? 'pl-2' : 'pr-2'
+  );
+
+  return (
+    <label className={labelClasses} {...props}>
+      {!right && <span className={labelInnnerClasses}>{children}</span>}
+      <input
+        type="checkbox"
+        className="absolute hidden w-full h-full -translate-x-1/2 rounded-md appearance-none left-1/2 peer"
+        checked={checked}
+        onChange={onChange}
+        disabled={disabled}
+        required={required}
+        {...inputsProps}
+      />
+      <span className={spanClasses}></span>
+      {right && <span className={labelInnnerClasses}>{children}</span>}
+    </label>
+  );
+}
 
 Switch.displayName = 'Switch';
 Switch.defaultProps = {
